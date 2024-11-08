6302a19d72580fd509464523dfd3b54e595fee58
Aggrey Maphutha <maphuthaaggrey83@gmail.com>
2024-11-08 18:34:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const categoryList = document.querySelector('.categorylst');
 const priceBtn = document.querySelector('.pricebtn');
 const priceList = document.querySelector('.pricelst');
 const closeListItems = document.querySelectorAll('ul.categorylst li, ul.pricelst li');
-const closeList = document.querySelectorAll('main p, div.food-price');
+const closeList = document.querySelectorAll('main .heading, div.food-price');
 
 
 closeList.addEventListener('click', () => {
