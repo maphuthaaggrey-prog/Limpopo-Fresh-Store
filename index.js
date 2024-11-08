19becbd5e20b5ac2704d0b2ab0d9f9a3ad7e1fdd
Aggrey Maphutha <maphuthaaggrey83@gmail.com>
2024-11-08 18:13:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const priceList = document.querySelector('.pricelst');
 const closeListItems = document.querySelectorAll('ul.categorylst li, ul.pricelst li');
 const closeList = document.querySelectorAll('.heading .food-price');
 
-closeList.AddEventListener('click', () {
+closeList.AddEventListener('click', () => {
     categoryList.classList.remove('showDrop');
     priceList.classList.remove('showDrop');
 });
